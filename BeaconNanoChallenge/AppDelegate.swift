5b6fb2c99777364c9b23051785e3b92be9028c24
Gabriela de Carvalho Barros Bezerra <Gabrieladecarvalhobezerra@gmail.com>
2017-03-23 15:48:43 -0300

    Delegates

--- BeaconNanoChallenge/AppDelegate.swift
+++ BeaconNanoChallenge/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import KontaktSDK
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Kontakt.setAPIKey("CgutVARXTneIgiHMilIvywIJTtlQzOhh")
         return true
     }
 
--- BeaconNanoChallenge/ViewController.swift
+++ BeaconNanoChallenge/ViewController.swift
@@ -7,71 +7,89 @@
 //
 
 import UIKit
+import CoreLocation
 import KontaktSDK
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
     
     var beaconManager: KTKBeaconManager!
     
+    //let myProximityUuid = UUID(uuidString: "E063A66C-146F-42B1-AC3C-36BA3640B7ED")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         beaconManager = KTKBeaconManager(delegate: self)
+      
+        beaconManager.stopMonitoringForAllRegions()
         
-        let myProximityUuid = UUID(uuidString: "E063A66C-146F-42B1-AC3C-36BA3640B7ED")
+        let myProximityUuid = UUID(uuidString: "F7826DA6-4FA2-4E98-8024-BC5B71E0893E")
         let region = KTKBeaconRegion(proximityUUID: myProximityUuid!, identifier: "Beacon region 1")
         
         switch KTKBeaconManager.locationAuthorizationStatus() {
         case .notDetermined:
             beaconManager.requestLocationAlwaysAuthorization()
-        case .denied, .restricted: break
-        // No access to Location Services
-        case .authorizedWhenInUse: break
-            // For most iBeacon-based app this type of
-        // permission is not adequate
         case .authorizedAlways:
             if KTKBeaconManager.isMonitoringAvailable() {
                 beaconManager.startMonitoring(for: region)
             }
+        case .denied, .restricted: break
+        case .authorizedWhenInUse: break
         }
-        
-    }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Kontakt.setAPIKey("CgutVARXTneIgiHMilIvywIJTtlQzOhh")
-        return true
     }
     
 }
 
 extension ViewController: KTKBeaconManagerDelegate {
-    func beaconManager(_ manager: KTKBeaconManager, didChangeLocationAuthorizationStatus status: CLAuthorizationStatus) {
+    
+ /*   func beaconManager(_ manager: KTKBeaconManager, didChangeLocationAuthorizationStatus status: CLAuthorizationStatus) {
+
+        let myProximityUuid = UUID(uuidString: "A1EA8130-0E1B-D4A1-B840-63F88C8DA1EA")
+        let region = KTKBeaconRegion(proximityUUID: myProximityUuid!, identifier: "Beacon region 1")
+        
         if status == .authorizedAlways {
-            // When status changes to CLAuthorizationStatus.authorizedAlways
-            // e.g. after calling beaconManager.requestLocationAlwaysAuthorization()
-            // we can start region monitoring from here
-            
+            if KTKBeaconManager.isMonitoringAvailable() {
+                beaconManager.startMonitoring(for: region)
+            }
         }
     }
-}
-
-extension ViewController {
-    
+ */
     
     func beaconManager(_ manager: KTKBeaconManager, didStartMonitoringFor region: KTKBeaconRegion) {
-        print(region)
-        print("Started Monitoring\n")
+        
+        label.text = "Monitoring..."
+        print("Started Monitoring the following regions:")
+        print(beaconManager.monitoredRegions)
+    }
+    
+    func beaconManager(_ manager: KTKBeaconManager, monitoringDidFailFor region: KTKBeaconRegion?, withError error: Error?) {
+        print("failed")
+        label.text = "Failed"
     }
     
     func beaconManager(_ manager: KTKBeaconManager, didEnter region: KTKBeaconRegion) {
         // Decide what to do when a user enters a range of your region; usually used
         // for triggering a local notification and/or starting a beacon ranging
+        manager.startRangingBeacons(in: region)
         print("Entered region")
+        label.text = "Entered  \(region)"
     }
     
     func beaconManager(_ manager: KTKBeaconManager, didExitRegion region: KTKBeaconRegion) {
         // Decide what to do when a user exits a range of your region; usually used
         // for triggering a local notification and stoping a beacon ranging
-        print("Leaved Region")
+        manager.stopRangingBeacons(in: region)
+        print("Left Region")
+        label.text = "Left \(region)"
+    }
+    
+    func beaconManager(_ manager: KTKBeaconManager, didRangeBeacons beacons: [CLBeacon], in region: KTKBeaconRegion) {
+        print("didRange")
+    }
+    
+    func beaconManager(_ manager: KTKBeaconManager, rangingBeaconsDidFailFor region: KTKBeaconRegion?, withError error: Error?) {
+        print(region!)
+        print("Failed")
     }
 }
